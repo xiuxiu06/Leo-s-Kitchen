f9ff89c34ac18ba146a31dcfd78c1c72f0ba3596
Tam Le <lengoctam0106@gmail.com>
2025-03-02 00:53:18 -0500

    Update

---
+++ pages/My_Recipe.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("My Recipes")
+st.write("Here, you can find all your saved recipes!")
+
+# --- SIDEBAR NAVIGATION ---
+# st.sidebar.title("Navigation")
+# st.sidebar.page_link("Home.py", label="🏠 Home")
+# st.sidebar.page_link("pages/About_Leo.py", label="ℹ️ About Leo")
+# st.sidebar.page_link("pages/My_Recipe.py", label="📊 My Recipes")
+# st.sidebar.page_link("pages/Leo_Chat_Bot.py", label="🤖 Chat Bot")
+# st.sidebar.page_link("pages/Share_Your_Meal.py", label="📝 Share Your Meal")
+# st.sidebar.page_link("pages/My_Profile.py", label="👤 My Profile")
+# st.sidebar.page_link("pages/Authentication.py", label="🔑 Login/Register")
--- pages/my_recipes.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("My Recipes")
-st.write("Here, you can find all your saved recipes!")
